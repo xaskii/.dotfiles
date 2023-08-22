8c93dfc50a07863b58119f3bf44101b9379f6ff7
Xavi <xavisimpson04@gmail.com>
2023-08-22 09:54:37 -0400

    added smartcase settings to set vim

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -20,6 +20,10 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- smart searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
