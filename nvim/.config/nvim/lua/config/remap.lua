3851c1062ff3e3ef23371c632c91bb25dc1ff71f
Xavi <xavisimpson04@gmail.com>
2023-08-25 19:28:44 -0400

    telescope fzf + other stuff

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,13 +1,21 @@
+local silent = { silent = true, noremap = true }
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- moving lines up and down in visual
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- leave cursor in same place
 vim.keymap.set("n", "J", "mzJ`z")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- save haha
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", silent)
 
 -- next greatest remap ever : asbjornHaland
 -- copying to system clipboard
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,22 +3,24 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+      },
     },
     config = function(_, opts)
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>ps', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > " ) })
-      end)
-
+      vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
       local telescope = require('telescope')
       telescope.setup {
-        -- pickers = {
-        --   find_files = {
-        --     hidden = true
-        --   }
-        -- }
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = 'smart_case',
+        },
       }
     end
   },
