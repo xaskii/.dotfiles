8b6f4573dd24d23ea30363ba11e02d5afb3298f2
Xavi <xavisimpson04@gmail.com>
2023-11-21 13:41:02 -0500

    updated more neovim stuff + alias stuff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -55,8 +55,6 @@ return {
       local lsp = require('lsp-zero')
 
       lsp.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
         lsp.default_keymaps({buffer = bufnr})
       end)
 
